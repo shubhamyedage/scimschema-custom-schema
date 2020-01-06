3332c30600d3ca103f73c313a37acd06d5d1f416
shubhamyedage <shubham.yedage@synerzip.com>
2020-01-06 17:33:14 +0530

    Updated with custom json

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-import extension
-from scimschema import validate, load_dict_to_schema
 import json
+
+from scimschema import core_schemas
+from scimschema._model.schema_response import ScimResponse
+
+import extension
+
+
 def get_data():
     with open("myuser.txt") as jsonfile:
         return json.load(jsonfile)
 
+ScimResponse(data=get_data(), core_schema_definitions=core_schemas.schema,
+             extension_schema_definitions=extension.schema).validate()
 
-validate(data=get_data(), extension_schema_definitions=extension.schema)
-
-
+# validate(data=get_data(), extension_schema_definitions=extension.schema)
 
 # schema = load_dict_to_schema("extension/myuser.json")
 # validate(
